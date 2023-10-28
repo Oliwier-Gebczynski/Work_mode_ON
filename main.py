9a962317cd861c0b79d94a6084d7758889b86005
Oliwier-Gebczynski <oliwiergebczynski@gmail.com>
2023-10-28 17:35:33 +0200

    some upgrades

--- main.py
+++ main.py
@@ -1,29 +1,37 @@
+import os
 import subprocess
-
 
 def read_from_file(fileName):
     try:
         with open(fileName, 'r') as file:
             lines = file.readlines()
 
-        ide_path = (lines[0].split('"'))[1]
-        first_url = (lines[1].split('"'))[1]
-        second_url = (lines[2].split('"'))[1]
-        spotify_path = (lines[3].split('"'))[1]
-        spotify_playlist = (lines[4].split('"'))[1]
+        list = []
 
-        list = [ide_path, first_url, second_url, spotify_path, spotify_playlist]
+        for line in lines:
+            if line.split('"')[0] == "app = ":
+                x = [line.split('"')[1], 1]
+                print(x[0])
+                print(x[1])
+                list.append(x)
+            else:
+                x = [line.split('"')[1], 0]
+                print(x[0])
+                print(x[1])
+                list.append(x)
 
         return list
+
     except FileNotFoundError:
         print("File error!")
 
 
 result = read_from_file("configuration.txt")
 
-subprocess.Popen([result[0]])
+for process in result:
+    if process[1] == 0:
+        subprocess.Popen(['firefox', process[0]])
+    else:
+        subprocess.Popen(process[0])
 
-subprocess.Popen(['firefox', result[1]])
-subprocess.Popen(['firefox', result[2]])
 
-subprocess.Popen([result[3], f'--uri=spotify:playlist:{result[4]}'])
